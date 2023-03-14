dd6fcd78fe791948c076d26de661937696914679
Ell534 <elliot94b@gmail.com>
2023-03-14 16:42:33 +0000

    add functionality to view a single review with basic grid layout applied

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Reviews from './components/Reviews';
+import SingleReview from './components/SingleReview';
 import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -14,8 +15,34 @@ function App() {
       <Header />
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home categories={categories} setCategories={setCategories} />} />
-        <Route path="/reviews" element={<Reviews categories={categories} setCategories={setCategories} isLoading={isLoading} setIsLoading={setIsLoading} />} />
+        <Route
+          path="/"
+          element={
+            <Home categories={categories} setCategories={setCategories} />
+          }
+        />
+        <Route
+          path="/reviews"
+          element={
+            <Reviews
+              categories={categories}
+              setCategories={setCategories}
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
+            />
+          }
+        />
+        <Route
+          path="/reviews/:review_id"
+          element={
+            <SingleReview
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
+              categories={categories}
+              setCategories={setCategories}
+            />
+          }
+        />
       </Routes>
     </div>
   );
