64281931bd83cb03c63088241c77c83b0a128541
Ell534 <elliot94b@gmail.com>
2023-03-16 10:30:07 +0000

    Added check for no comments, with a relevant message to be displayed

--- src/components/CommentsCards.jsx
+++ src/components/CommentsCards.jsx
@@ -1,39 +1,44 @@
 import { useState, useEffect } from 'react';
 import { getCommentsByReviewId } from '../utils';
 
-const CommentsCards = ({ review_id}) => {
+const CommentsCards = ({ review_id }) => {
   const [commentsList, setCommentsList] = useState([]);
   const [commentsLoading, setCommentsLoading] = useState(true);
 
   useEffect(() => {
-    setCommentsLoading(true)
+    setCommentsLoading(true);
     getCommentsByReviewId(review_id).then((commentsFromApi) => {
       setCommentsList(commentsFromApi);
       setCommentsLoading(false);
     });
   }, []);
 
-  console.log(commentsList);
-
   if (commentsLoading) {
-    return <h4>Comments are loading, please wait...</h4>
+    return <h4>Comments are loading, please wait...</h4>;
+  }
+
+  if (commentsList.length === 0) {
+    return (
+      <p>
+        There are currently no comments for this review, maybe you should be the
+        first to add one!
+      </p>
+    );
   }
 
   return (
-    <>
-      <ul>
-        {commentsList.map((comment) => {
-          return (
-            <li key={comment.comment_id}>
-              <h4>Comment author: {comment.author}</h4>
-              <p>Comment: {comment.body}</p>
-              <p>Votes: {comment.votes}</p>
-              <p>**placeholder** (+1 vote) (-1 vote) (delete) **placeholder**</p>
-            </li>
-          )
-        })}
-      </ul>
-    </>
+    <ul>
+      {commentsList.map((comment) => {
+        return (
+          <li key={comment.comment_id}>
+            <h4>Comment author: {comment.author}</h4>
+            <p>Comment: {comment.body}</p>
+            <p>Votes: {comment.votes}</p>
+            <p>**placeholder** (+1 vote) (-1 vote) (delete) **placeholder**</p>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
